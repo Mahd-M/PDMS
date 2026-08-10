6db034b2d5207691e0ce856701c3fa5491eb0869
Mahd <mmahd7186@gmail.com>
2026-08-10 11:45:44 +0500

    Wire up Wanted Criminals page URL route and nav link

--- records/urls.py
+++ records/urls.py
@@ -6,6 +6,7 @@ app_name = "records"
 urlpatterns = [
     path("", views.record_list, name="record_list"),
     path("new/", views.record_create, name="record_create"),
+    path("wanted/", views.wanted_list, name="wanted_list"),
     path("<int:pk>/", views.record_detail, name="record_detail"),
     path("<int:pk>/edit/", views.record_edit, name="record_edit"),
     path("<int:record_pk>/court-dates/new/", views.court_date_create, name="court_date_create"),
